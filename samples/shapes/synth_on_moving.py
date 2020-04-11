1b3442d40eefffd678b8ad0f2914770bef9ebcc1
itamar gruber <itamar.gru@gmail.com>
2020-04-11 17:14:12 +0300

    fixed the output image color (from BGR to RGB) commented the validation and assigned 0 to number of val images, to perform only the synthetic dataset

--- samples/shapes/synth_on_moving.py
+++ samples/shapes/synth_on_moving.py
@@ -105,6 +105,7 @@ class ShapesDataset(utils.Dataset):
             image = self.draw_shape(image, shape, dims, color)
         image_path = os.path.join(self.PATH_SAVE_DATA, "train", '{:05d}'.format(image_id))
         image_path += ".jpeg"
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         cv2.imwrite(image_path, image)
         return image
 
@@ -241,7 +242,7 @@ if __name__ == "__main__":
 
     # Validation dataset
     dataset_val = ShapesDataset()
-    num_images_val = 50
+    num_images_val = 0
     dataset_val.load_shapes(num_images_val, config.IMAGE_SHAPE[0], config.IMAGE_SHAPE[1])
     dataset_val.prepare()
 
@@ -274,46 +275,46 @@ if __name__ == "__main__":
                 layers='heads')
 
 
-    class InferenceConfig(ShapesConfig):
-        GPU_COUNT = 1
-        IMAGES_PER_GPU = 1
-
-
-    inference_config = InferenceConfig()
-
-    # Recreate the model in inference mode
-    model = modellib.MaskRCNN(mode="inference",
-                              config=inference_config,
-                              model_dir=MODEL_DIR)
-
-    # Get path to saved weights
-    # Either set a specific path or find last trained weights
-    # model_path = os.path.join(ROOT_DIR, ".h5 file name here")
-    model_path = model.find_last()
-
-    # Load trained weights
-    print("Loading weights from ", model_path)
-    model.load_weights(model_path, by_name=True)
-
-    # Test on a random image
-    NUM_LOOP = 5
-    for _ in range(NUM_LOOP):
-        image_id = random.choice(dataset_val.image_ids)
-        original_image, image_meta, gt_class_id, gt_bbox, gt_mask = \
-            modellib.load_image_gt(dataset_val, inference_config,
-                                   image_id, use_mini_mask=False)
-
-        log("original_image", original_image)
-        log("image_meta", image_meta)
-        log("gt_class_id", gt_class_id)
-        log("gt_bbox", gt_bbox)
-        log("gt_mask", gt_mask)
-
-        visualize.display_instances(original_image, gt_bbox, gt_mask, gt_class_id,
-                                    dataset_train.class_names, figsize=(8, 8))
-
-        results = model.detect([original_image], verbose=1)
-
-        r = results[0]
-        visualize.display_instances(original_image, r['rois'], r['masks'], r['class_ids'],
-                                    dataset_val.class_names, r['scores'])  # , ax=get_ax())
+    # class InferenceConfig(ShapesConfig):
+    #     GPU_COUNT = 1
+    #     IMAGES_PER_GPU = 1
+    #
+    #
+    # inference_config = InferenceConfig()
+    #
+    # # Recreate the model in inference mode
+    # model = modellib.MaskRCNN(mode="inference",
+    #                           config=inference_config,
+    #                           model_dir=MODEL_DIR)
+    #
+    # # Get path to saved weights
+    # # Either set a specific path or find last trained weights
+    # # model_path = os.path.join(ROOT_DIR, ".h5 file name here")
+    # model_path = model.find_last()
+    #
+    # # Load trained weights
+    # print("Loading weights from ", model_path)
+    # model.load_weights(model_path, by_name=True)
+    #
+    # # Test on a random image
+    # NUM_LOOP = 5
+    # for _ in range(NUM_LOOP):
+    #     image_id = random.choice(dataset_val.image_ids)
+    #     original_image, image_meta, gt_class_id, gt_bbox, gt_mask = \
+    #         modellib.load_image_gt(dataset_val, inference_config,
+    #                                image_id, use_mini_mask=False)
+    #
+    #     log("original_image", original_image)
+    #     log("image_meta", image_meta)
+    #     log("gt_class_id", gt_class_id)
+    #     log("gt_bbox", gt_bbox)
+    #     log("gt_mask", gt_mask)
+    #
+    #     visualize.display_instances(original_image, gt_bbox, gt_mask, gt_class_id,
+    #                                 dataset_train.class_names, figsize=(8, 8))
+    #
+    #     results = model.detect([original_image], verbose=1)
+    #
+    #     r = results[0]
+    #     visualize.display_instances(original_image, r['rois'], r['masks'], r['class_ids'],
+    #                                 dataset_val.class_names, r['scores'])  # , ax=get_ax())
